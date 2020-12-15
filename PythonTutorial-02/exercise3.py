f8776019092e0aa590614d10314e1759ace28656
yhchuang <yhchuang@realtek.com>
2020-12-15 19:25:40 +0800

    Turotial3 exercise3

--- PythonTutorial-02/exercise3.py
+++ PythonTutorial-02/exercise3.py
@@ -1,5 +1,18 @@
 
 def ListToDecimal(L):
+    result = 0
+    L_len = len(L)
+    for i in range(L_len):
+        result += L[i] * 10 ** (L_len - i - 1)
 
-    for i in L:
-        i 
+    return result
+
+def AddTwoLists(L1, L2):
+    N1 = ListToDecimal(L1)
+    N2 = ListToDecimal(L2)
+    return N1 + N2
+
+L1 = [5, 2, 3]
+L2 = [1, 0, 2, 4]
+
+print(AddTwoLists(L1, L2))
